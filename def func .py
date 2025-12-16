0cb221bf512f6eb32ad5367f9abcb0a59a8d87b4
Bibek <mr.bibekdahal@gmail.com>
2025-12-16 21:45:36 +0545

    Add files via upload

---
+++ def func .py
@@ -0,0 +1,15 @@
+#Variables that are created outside of a function are known as global variables.
+#Global variables can be used by everyone, both inside of functions and outside.
+
+"Note #Global variables can be used by everyone, both inside of functions and outside."
+
+
+x = "Beautiful" #variable is defined outside the function ( Global Variables)
+
+def myfunction():
+    print("You are " + x ,"!!!")
+
+myfunction() #call function
+
+
+
